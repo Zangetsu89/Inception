b52abd0f97edc9c46d4502b65429e4018b4e4b0e
Essam <[essam.dawood@tomtom.com]>
2023-08-27 10:01:39 +0200

    pushed inception

---
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+sed -i 's|MYSQL_DB|'${MYSQL_DB}'|g' /tmp/init.sql
+sed -i 's|MYSQL_USER|'${MYSQL_USER}'|g' /tmp/init.sql
+sed -i 's|MYSQL_PWD|'${MYSQL_PWD}'|g' /tmp/init.sql
+sed -i 's|MYSQL_ROOT_PWD|'${MYSQL_ROOT_PWD}'|g' /tmp/init.sql
+sed -i 's|MYSQL_PORT|3306|g' /etc/mysql/my.cnf
+sed -i 's|MYSQL_ADDRESS|0.0.0.0|g' /etc/mysql/my.cnf
+
+if [ -d "/var/lib/mysql/$MYSQL_DB" ]
+
+then
+  echo "Database already exists."
+  mysqld_safe
+
+else
+  mysql_install_db
+  mysqld --init-file="/tmp/init.sql"
+
+fi
+
+exec "$@"
